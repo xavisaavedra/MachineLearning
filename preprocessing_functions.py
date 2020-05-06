adbad27bd01fbd832950f4b675d31ac27d314cf6
Xavi Saavedra Alois <xavi.saavedra.alois@gmail.com>
2020-05-06 21:57:28 +0200

    Añadir nuevas funciones

--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -5,6 +5,8 @@ import pathlib
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import PowerTransformer
+
 import math
 
 import matplotlib.pyplot as plt
@@ -142,6 +144,62 @@ def analisis_num_val(df):
 	sns.boxplot(df, color="black", boxprops=dict(alpha=.3), width=0.3);
 	interpreting_skew(df)
 	interpreting_kurtosis(df)
+
+
+# Function to calculate missing values by column
+def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
+
+
+
+""" Detectamos valores 'Outliers'. Esta funcion nos devuelve los indices en el dataframe
+    De los esos valores """
+
+def drop_outliers(df):
+    quartile_1, quartile_3 = np.percentile(df, [25, 75])
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 - (iqr * 1.5)
+    upper_bound = quartile_3 + (iqr * 1.5)
+    return np.where((df > upper_bound) | (df < lower_bound))
+
+
+
+def apply_yeojohnson(df):
+    feature = pd.DataFrame(df)
+    name = feature.columns
+    print(name)
+    pt = PowerTransformer(method='yeo-johnson', standardize=True,)
+    tr_yeo = pt.fit_transform(feature)
+    return pd.DataFrame(tr_yeo, columns=name)
+
+
+
+
+
 
 
 
