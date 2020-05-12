4ae9a29ab9805a408abf242a1f470fd9506c515b
Xavi Saavedra Alois <xavi.saavedra.alois@gmail.com>
2020-05-12 20:30:50 +0200

    Varios temas sin importancia

--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -23,12 +23,10 @@ import joblib
 # ================================================
 
 
-
 # check the file encoding and load it into a pandas dataframe
 # ===========================================================
 
 def load_dataset(*, file_name: str) -> pd.DataFrame:
-
     """Load a persisted pipeline."""
     print("File".ljust(45), "Encoding")
     filename = f"{config.DATASET_DIR}/{file_name}"
@@ -42,142 +40,130 @@ def load_dataset(*, file_name: str) -> pd.DataFrame:
     return _data
 
 
-
-
 def get_numerical_vars(df):
-	
-	# make list of numerical variables
-	num_vars = [var for var in df.columns if df[var].dtypes != 'O']
-	# print('Número de variables numéricas: ', len(num_vars))
 
-	# visualise the numerical variables
-	return df[num_vars]
+        # make list of numerical variables
+    num_vars = [var for var in df.columns if df[var].dtypes != 'O']
+    # print('Número de variables numéricas: ', len(num_vars))
 
-
+    # visualise the numerical variables
+    return df[num_vars]
 
 
 def get_discrete_vars(df):
 
-	num_vars = get_numerical_vars(df)
-	#  let's male a list of discrete variables
-	discrete_vars = [var for var in num_vars if len(
-    	df[var].unique()) < 20]
+    num_vars = get_numerical_vars(df)
+    #  let's male a list of discrete variables
+    discrete_vars = [var for var in num_vars if len(
+        df[var].unique()) < 20]
 
-	# print('Número de variables discretas: ', len(discrete_vars))
-	return df[discrete_vars]
-
-
+    # print('Número de variables discretas: ', len(discrete_vars))
+    return df[discrete_vars]
 
 
 def get_continous_vars(df):
-	# make list of continuous variables
-	num_vars = get_numerical_vars(df)
-	discrete_vars = get_discrete_vars(df)
-	cont_vars = [
-    	var for var in num_vars if var not in discrete_vars]
+    # make list of continuous variables
+    num_vars = get_numerical_vars(df)
+    discrete_vars = get_discrete_vars(df)
+    cont_vars = [
+        var for var in num_vars if var not in discrete_vars]
 
-	# print('Number of continuous variables: ', len(cont_vars))
-	return df[cont_vars]
-
-
+    # print('Number of continuous variables: ', len(cont_vars))
+    return df[cont_vars]
 
 
 def get_cat_vars(df):
-	# capture categorical variables in a list
-   cat_vars = [var for var in df.columns if df[var].dtypes == 'O']
+    # capture categorical variables in a list
+    cat_vars = [var for var in df.columns if df[var].dtypes == 'O']
 
-   # print('Number of categorical variables: ', len(cat_vars))
-   return df[cat_vars]
+    # print('Number of categorical variables: ', len(cat_vars))
+    return df[cat_vars]
 
 
 def redefine_columns(df):
-	# Map the lowering function to all column names
-	df.columns = map(str.lower, df.columns)
-	df.columns = df.columns.str.replace(' ', '')
-	print(f'Columnas: {list(df.columns)}')
-	return df
+    # Map the lowering function to all column names
+    df.columns = map(str.lower, df.columns)
+    df.columns = df.columns.str.replace(' ', '')
+    print(f'Columnas: {list(df.columns)}')
+    return df
 
 
 def interpreting_skew(df):
-	#skewness
-	skew = df.skew()
+    # skewness
+    skew = df.skew()
 
-	#Interpreting Skew 
-	if -0.5 < skew < 0.5:
-		print(f'Un sesgo de {skew} significa que la distribución es  aprox. simétrica')
-	elif  -0.5 < skew < -1.0 or 0.5 < skew < 1.0:
-		print(f'Un sesgo de {skew} significa que la distribución es moderadamente sesgada')
-	else:
-		print (f'Un sesgo de {skew} significa que la distribución está muy sesgada')
-
+    # Interpreting Skew
+    if -0.5 < skew < 0.5:
+        print(f'Un sesgo de {skew} significa que la distribución es  aprox. simétrica')
+    elif -0.5 < skew < -1.0 or 0.5 < skew < 1.0:
+        print(f'Un sesgo de {skew} significa que la distribución es moderadamente sesgada')
+    else:
+        print (f'Un sesgo de {skew} significa que la distribución está muy sesgada')
 
 
 def interpreting_kurtosis(df):
-	kurtosis = df.kurtosis()
+    kurtosis = df.kurtosis()
 
-#Interpreting Kurtosis
-	if  -0.5 < kurtosis < 0.5:
-		print (f'Una curtosis de {kurtosis} significa que la distribución es aproximadamente normal.')
-	elif kurtosis <= -0.5: 
-		print (f'Una curtosis de {kurtosis} significa que la distribución es de cola ligera (negativa).')
-	elif kurtosis >= 0.5:
-		print (f'Una curtosis de {kurtosis} significa que la distribución es de cola gruesa (positiva).')
+# Interpreting Kurtosis
+    if -0.5 < kurtosis < 0.5:
+        print (f'Una curtosis de {kurtosis} significa que la distribución es aproximadamente normal.')
+    elif kurtosis <= -0.5:
+        print (f'Una curtosis de {kurtosis} significa que la distribución es de cola ligera (negativa).')
+    elif kurtosis >= 0.5:
+        print (f'Una curtosis de {kurtosis} significa que la distribución es de cola gruesa (positiva).')
 
 
 def analisis_num_val(df):
+    plt.figure(figsize=(20, 6))
 
-	plt.figure(figsize=(14,3))
+    # Creamos el primer grafico con este formato plt.subplot(filas, columnas, No de grafico)
+    plt.subplot(131)
+    # Density Plot and Histogram of all arrival delays
+    sns.distplot(df, hist=True, kde=True,
+        bins='auto',
+        hist_kws={'edgecolor': 'black'},
+        kde_kws={'linewidth': 4});
 
-	#Creamos el primer grafico con este formato plt.subplot(filas, columnas, No de grafico)
-	plt.subplot(131)
-	# Density Plot and Histogram of all arrival delays
-	sns.distplot(df, hist=True, kde=True, 
-             bins='auto', 
-             hist_kws={'edgecolor':'black'},
-             kde_kws={'linewidth': 4});
+    plt.subplot(132)
+    sns.distplot(df, hist=False, kde=True,
+        kde_kws={'shade': True, 'linewidth': 3});
 
-	plt.subplot(132)
-	sns.distplot(df, hist = False, kde = True,
-                 kde_kws = {'shade': True, 'linewidth': 3});
-
-	plt.subplot(133)
-	sns.boxplot(df, color="black", boxprops=dict(alpha=.3), width=0.3);
-	interpreting_skew(df)
-	interpreting_kurtosis(df)
+    plt.subplot(133)
+    sns.boxplot(y=df);
 
 
 # Function to calculate missing values by column
 def missing_values_table(df):
-        # Total missing values
-        mis_val = df.isnull().sum()
-        
-        # Percentage of missing values
-        mis_val_percent = 100 * df.isnull().sum() / len(df)
-        
-        # Make a table with the results
-        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
-        
-        # Rename the columns
-        mis_val_table_ren_columns = mis_val_table.rename(
-        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
-        
-        # Sort the table by percentage of missing descending
-        mis_val_table_ren_columns = mis_val_table_ren_columns[
-            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
-        '% of Total Values', ascending=False).round(1)
-        
-        # Print some summary information
-        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
-            "There are " + str(mis_val_table_ren_columns.shape[0]) +
-              " columns that have missing values.")
-        
-        # Return the dataframe with missing information
-        return mis_val_table_ren_columns
+    # Total missing values
+    mis_val = df.isnull().sum()
 
+    # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+
+    # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+
+    # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(
+        columns={0: 'Missing Values', 1: '% of Total Values'})
+
+    # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+
+    # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+           "There are " + str(mis_val_table_ren_columns.shape[0]) +
+           " columns that have missing values.")
+
+    # Return the dataframe with missing information
+    return mis_val_table_ren_columns
 
 
 """ Detectamos valores 'Outliers'. Esta funcion nos devuelve los indices en el dataframe
     De los esos valores """
+
 
 def drop_outliers(df):
     quartile_1, quartile_3 = np.percentile(df, [25, 75])
@@ -187,7 +173,6 @@ def drop_outliers(df):
     return np.where((df > upper_bound) | (df < lower_bound))
 
 
-
 def apply_yeojohnson(df):
     feature = pd.DataFrame(df)
     name = feature.columns
@@ -195,18 +180,3 @@ def apply_yeojohnson(df):
     pt = PowerTransformer(method='yeo-johnson', standardize=True,)
     tr_yeo = pt.fit_transform(feature)
     return pd.DataFrame(tr_yeo, columns=name)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
